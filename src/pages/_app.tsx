ac64a968c77f76df6473ce0e67ee1bcc5250024f
Nathallye Bacelar <nathallyet@gmail.com>
2023-05-09 21:54:23 -0300

    Image - Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Image from "next/image";
 
 import { globalStyles } from "../styles/global";
 import { Container, Header } from "../styles/pages/app";
@@ -11,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} alt="" width={100} height={100} />
       </Header>
       
       <Component {...pageProps} />
