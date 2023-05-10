0b85638281e65eae08536fee760ba32a7b1253ba
Nathallye Bacelar <nathallyet@gmail.com>
2023-05-09 21:56:26 -0300

    Alter function for arrow function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Container>
       <Header>
-        <Image src={logoImg.src} alt="" width={100} height={100} />
+        <Image src={logoImg.src} alt="" width={130} height={52} />
       </Header>
       
       <Component {...pageProps} />
