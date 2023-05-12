0f8e641900e38733b034ced507b7f02a0b6f644a
Nathallye Bacelar <nathallyet@gmail.com>
2023-05-12 20:43:57 -0300

    GetServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import shirt1 from "../assets/shirts/shirt-1.png";
 import shirt2 from "../assets/shirts/shirt-2.png";
 import shirt3 from "../assets/shirts/shirt-3.png";
 
-const Home = () => {
+const Home = (props) => {
   const [sliderRef] = useKeenSlider({ // ref é uma funcionalidade do React, que nos permite ter acesso a uma referência de um elemento na DOM - Conseguimos modificar o funcionamento desse elemento e estilização diretamente pelo JS/TS (igual document.getElementById)
     slides: {
       perView: 3,
@@ -18,6 +18,8 @@ const Home = () => {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
+
       <Product className="keen-slider__slide">
         <Image alt="" src={shirt1} width={520} height={480} />
 
@@ -58,3 +60,16 @@ const Home = () => {
 };
 
 export default Home;
+
+
+export const getServerSideProps = async () => { // o next não devolve nada para o front end antes que tudo esteja carregado (principio de aplicações SSR)
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  console.log("rodou"); // só aparece no console do node(terminal)
+
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
+}
