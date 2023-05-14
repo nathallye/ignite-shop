2323f6f595534411ec4d0f40a58ee95a7da6a534
Nathallye Bacelar <nathallyet@gmail.com>
2023-05-13 23:40:26 -0300

    Config Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
-
+import Stripe from "stripe";
+import { GetServerSideProps } from "next";
 import { useKeenSlider } from "keen-slider/react";
+
+import { stripe } from "../lib/stripe";
 
 import { HomeContainer, Product } from "../styles/pages/home";
 
@@ -8,8 +11,18 @@ import shirt1 from "../assets/shirts/shirt-1.png";
 import shirt2 from "../assets/shirts/shirt-2.png";
 import shirt3 from "../assets/shirts/shirt-3.png";
 
-const Home = (props) => {
-  const [sliderRef] = useKeenSlider({ // ref é uma funcionalidade do React, que nos permite ter acesso a uma referência de um elemento na DOM - Conseguimos modificar o funcionamento desse elemento e estilização diretamente pelo JS/TS (igual document.getElementById)
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
+}
+
+const Home = ({ products }: HomeProps) => {
+  const [sliderRef] = useKeenSlider({
+    // ref é uma funcionalidade do React, que nos permite ter acesso a uma referência de um elemento na DOM - Conseguimos modificar o funcionamento desse elemento e estilização diretamente pelo JS/TS (igual document.getElementById)
     slides: {
       perView: 3,
       spacing: 48
@@ -18,59 +31,48 @@ const Home = (props) => {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <pre>{JSON.stringify(props.list)}</pre>
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-      <Product className="keen-slider__slide">
-        <Image alt="" src={shirt1} width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image alt="" src={shirt2} width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image alt="" src={shirt3} width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image alt="" src={shirt2} width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        );
+      })}
     </HomeContainer>
   );
 };
 
 export default Home;
 
-
-export const getServerSideProps = async () => { // o next não devolve nada para o front end antes que tudo esteja carregado (principio de aplicações SSR)
+export const getServerSideProps: GetServerSideProps = async () => {
+  // o next não devolve nada para o front end antes que tudo esteja carregado (principio de aplicações SSR)
   // o código que colocarmos aqui dentro não ficar visível para o usuário final, desse modo, podemos colocar código sensível (código de autenticaçao, de banco de dados...)
-  await new Promise(resolve => setTimeout(resolve, 2000))
 
-  console.log("rodou"); // só aparece no console do node(terminal)
+  const response = await stripe.products.list({
+    expand: ["data.default_price"] // acessando a tabela de relacionada a tabela de products - default_price
+  });
+
+  // console.log(response.data); // só aparece no console do node(terminal)
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100
+    };
+  });
 
   return {
     props: {
-      list: [1, 2, 3]
+      products
     }
-  }
-}
+  };
+};
